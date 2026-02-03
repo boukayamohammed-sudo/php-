ac9d949b35a47c20297b454e868277b9e4453c92
boukayamohammed-sudo <boukayamohammed@gmail.com>
2026-02-03 12:26:06 +0100

    php

---
+++ Chapitre 4 – Tableaux simples et associatifs .php
@@ -0,0 +1,20 @@
+<?php
+$fruits = ["mohamed" ,"founti" , "zakaria"];
+echo $fruits[0]; 
+//===============================================//
+$utilisateur =[
+    "nom" => "mohamed",
+    "email" => "moha@gmail.com",
+    "age" => "23" 
+    
+];
+echo $utilisateur['age'];
+echo $utilisateur["email"];
+//====================================//
+$voiture = [
+    "marque" => "golf ",
+    "modele" => "golf 8 gtd",
+    "anneè" => "2023"
+];
+echo "modele :" .$voiture["modele"]
+?>
